d63ae6ccfa8eaec82684a73ba33bca220aca370f
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-08 12:03:18 -0600

    Write tests for #render in cell

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,50 +3,79 @@ require "./spec/spec_helper"
 RSpec.describe Cell do
 
   before(:each) do
-    @cell = Cell.new("B4")
+    @cell_1 = Cell.new("B4")
+    @cell_2 = Cell.new("C3")
     @cruiser = Ship.new("Cruiser", 3)
   end
 
   describe "#initialize" do
     it "exists" do
-      expect(@cell).to be_an_instance_of(Cell)
+      expect(@cell_1).to be_an_instance_of(Cell)
     end
 
     it "has attributes" do
-      expect(@cell.coordinate).to eq("B4")
-      expect(@cell.ship).to be nil
+      expect(@cell_1.coordinate).to eq("B4")
+      expect(@cell_1.ship).to be nil
     end
   end
 
   describe "#empty?" do
     it "defaults to true" do
-      expect(@cell.empty?).to be true
+      expect(@cell_1.empty?).to be true
     end
   end
 
   describe "#place_ship" do
     it "assigns ship attribute" do
-      expect(@cell.ship).to be_nil
-      expect(@cell.empty?).to be true
-      @cell.place_ship(@cruiser)
-      expect(@cell.ship).to eq(@cruiser)
-      expect(@cell.empty?).to be false
+      expect(@cell_1.ship).to be_nil
+      expect(@cell_1.empty?).to be true
+      @cell_1.place_ship(@cruiser)
+      expect(@cell_1.ship).to eq(@cruiser)
+      expect(@cell_1.empty?).to be false
     end
   end
 
   describe "#fire_upon and #fired_upon?" do
     it "fires on a cell and updates state" do
-      @cell.place_ship(@cruiser)
-      expect(@cell.fired_upon?).to be false
-      @cell.fire_upon
-      expect(@cell.fired_upon?).to be true
-      expect(@cell.ship.health).to eq(2)
+      @cell_1.place_ship(@cruiser)
+      expect(@cell_1.fired_upon?).to be false
+      @cell_1.fire_upon
+      expect(@cell_1.fired_upon?).to be true
+      expect(@cell_1.ship.health).to eq(2)
     end
 
     it "still functions if cell does not contain ship" do
-      expect(@cell.fired_upon?).to be false
-      @cell.fire_upon
-      expect(@cell.fired_upon?).to be true
+      expect(@cell_1.fired_upon?).to be false
+      @cell_1.fire_upon
+      expect(@cell_1.fired_upon?).to be true
     end
+  end
+
+  describe "#render" do
+    it "returns '.' if cell is empty and unfired upon" do
+      expect(@cell_1.render).to eq(".")
+    end
+
+    it "returns 'M' if cell is empty and fired upon" do
+      expect(@cell_1.render).to eq(".")
+      @cell_1.fire_upon
+      expect(@cell_1.render).to eq("M")
+    end
+
+    it "returns 'H' if cell is not empty and fired upon" do
+      expect(@cell_2.render).to eq(".")
+      @cell_2.place_ship(@cruiser)
+      expect(@cell_2.render).to eq(".")
+      @cell_2.fire_upon
+      expect(@cell_2.render).to eq("H")
+    end
+
+    it "returns 'X' if cell is fired upon and ship is sunk" do
+      expect(@cell_2.render).to eq(".")
+      @cell_2.place_ship(@cruiser)
+      expect(@cell_2.render).to eq(".")
+      @cell_2.ship.health = 1 # edit this test when ready
+      @cell_2.fire_upon
+      expect(@cell_2.render).to eq("X")
   end
 end
