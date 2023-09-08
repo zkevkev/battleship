2e1e1952408c1ec766394715f50d86a56c89880a
Sam Tran <samuelttran@gmail.com>
2023-09-08 16:54:24 -0600

    Write #valid_coordinate? test for Cell

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -26,4 +26,14 @@ RSpec.describe Board do
       end
     end
   end
+
+  describe "#valid_coordinate?" do
+    it "returns boolean result of coordinate test" do
+      expect(@board.valid_coordinate?('A1')).to be true
+      expect(@board.valid_coordinate?('D4')).to be true
+      expect(@board.valid_coordinate?('A5')).to be false
+      expect(@board.valid_coordinate?('E1')).to be false
+      expect(@board.valid_coordinate?('A22')).to be false
+    end
+  end
 end
