85cdf17b154b66478896ba3e390bdb9bfcbc1472
Sam Tran <samuelttran@gmail.com>
2023-09-10 13:34:02 -0600

    Create board and print

--- lib/battleship_runner.rb
+++ lib/battleship_runner.rb
@@ -1,11 +1,20 @@
+require "./spec/spec_helper"
+
 class Battleship
   def start
     puts "Welcome to BATTLESHIP"
     puts "Enter p to play. Enter q to quit."
     setup_response = gets.chomp
 
-    if setup_response == "q" || "Q"
-    elsif setup_response == "p" || "P"
+    if setup_response == "q" || setup_response == "Q"
+      puts 'Game over'
+    elsif setup_response == "p" || setup_response == "P"
+      new_board = Board.new
+      new_board.generate_cells
+      puts new_board.render
     end
   end
 end
+
+battleship = Battleship.new
+battleship.start
