8e18b75d89f08a85da3a4be2cffeddba1417ed70
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-10 07:04:34 -0600

    Write tests for #vertical_helper in board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -46,6 +46,16 @@ RSpec.describe Board do
       end
     end
 
+    describe "#vertical_helper" do
+      it "determines valid placement of ship by vertically consecutive coordinates" do
+        expect(@board.horizontal_helper(@cruiser, ["A1", "C2", "B3"])).to be false
+        expect(@board.horizontal_helper(@submarine, ["A1", "C1"])).to be false
+        # expect(@board.horizontal_helper(@cruiser, ["A3", "A2", "A1"])).to be false
+        expect(@board.horizontal_helper(@cruiser, ["A1", "B2", "C2"])).to be true
+        expect(@board.horizontal_helper(@cruiser, ["A1", "B1", "C1"])).to be true
+      end
+    end
+
     describe "#valid_placement?" do
       xit "determines valid placement of ship by length" do
         expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be false
