2d1d9eec7bf19c3ef094154c02c4f15a6cc7b899
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-10 07:30:11 -0600

    Refactor tests for #horizontal_helper and #vertical_helper in board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe Board do
       it "determines valid placement of ship by horizontally consecutive coordinates" do
         expect(@board.horizontal_helper(@cruiser, ["A1", "A2", "A4"])).to be false
         expect(@board.horizontal_helper(@submarine, ["A1", "C1"])).to be false
-        # expect(@board.horizontal_helper(@cruiser, ["A3", "A2", "A1"])).to be false
+        expect(@board.horizontal_helper(@cruiser, ["A3", "A2", "A1"])).to be false
         expect(@board.horizontal_helper(@cruiser, ["A1", "A2", "A3"])).to be true
       end
     end
@@ -50,8 +50,7 @@ RSpec.describe Board do
       it "determines valid placement of ship by vertically consecutive coordinates" do
         expect(@board.vertical_helper(@cruiser, ["A1", "C2", "B3"])).to be false
         expect(@board.vertical_helper(@submarine, ["A1", "C1"])).to be false
-        # expect(@board.vertical_helper(@cruiser, ["A3", "A2", "A1"])).to be false
-        expect(@board.vertical_helper(@cruiser, ["A1", "B2", "C2"])).to be false
+        expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be true
         expect(@board.vertical_helper(@cruiser, ["A1", "B1", "C1"])).to be true
       end
     end
